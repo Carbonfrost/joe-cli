4d86d8ca1d87c1dce51fdb8c012dd2eb6e9e2e3c
Gary Mann <garythemann@users.noreply.github.com>
2021-08-07 01:32:09 -0700

    Introduce Error API

---
+++ error.go
@@ -0,0 +1,103 @@
+package cli
+
+import (
+	"fmt"
+)
+
+type ErrorCode int
+
+type Error struct {
+	Code ErrorCode
+	Err  error
+	Name string
+}
+
+type ExitCoder interface {
+	error
+	ExitCode() int
+}
+
+type exitError struct {
+	message  interface{}
+	exitCode int
+}
+
+const (
+	UnexpectedArgument = ErrorCode(iota)
+)
+
+func Exit(message ...interface{}) ExitCoder {
+	switch len(message) {
+	case 0:
+		return exitCore("", 1)
+	case 1:
+		switch msg := message[0].(type) {
+		case ErrorCode:
+			return &Error{
+				Code: msg,
+			}
+		case ExitCoder:
+			return msg
+		case int:
+			return exitCore("", msg)
+		case nil:
+			return exitCore("", 1)
+		default:
+			return exitCore(fmt.Sprint(msg), 1)
+		}
+	default:
+		last := len(message) - 1
+		switch code := message[last].(type) {
+		case int:
+			return exitCore(fmt.Sprint(message[0:last]...), code)
+		case ErrorCode:
+			return exitCore(fmt.Sprintf("%s: %s", code.String(), fmt.Sprint(message[0:last]...)), 2)
+		default:
+			return exitCore(fmt.Sprint(message...), 1)
+		}
+	}
+}
+
+func exitCore(message string, code int) ExitCoder {
+	if message == "" {
+		message = fmt.Sprintf("exited with status %d", code)
+	}
+	return &exitError{
+		message:  message,
+		exitCode: code,
+	}
+}
+
+func (e *Error) ExitCode() int {
+	return 2
+}
+
+func (e *Error) Error() string {
+	if e.Err == nil {
+		return e.Code.String()
+	}
+	return e.Err.Error()
+}
+
+func (e ErrorCode) String() string {
+	switch e {
+	case UnexpectedArgument:
+		return "unexpected argument"
+	}
+	return "unknown error"
+}
+
+func (e *exitError) Error() string {
+	return fmt.Sprintf("%v", e.message)
+}
+
+func (e *exitError) ExitCode() int {
+	return e.exitCode
+}
+
+func unexpectedArgument(value string) *Error {
+	return &Error{
+		Code: UnexpectedArgument,
+		Err:  fmt.Errorf("too many arguments: %q", value),
+	}
+}
---
+++ error_test.go
@@ -0,0 +1,37 @@
+package cli_test
+
+import (
+	"errors"
+
+	"github.com/Carbonfrost/joe-cli"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
+)
+
+var _ = Describe("Exit", func() {
+	DescribeTable("arguments",
+		func(message interface{}, expectedMessage types.GomegaMatcher, expectedCode int) {
+			var err cli.ExitCoder
+			switch msg := message.(type) {
+			case []interface{}:
+				err = cli.Exit(msg...)
+			default:
+				err = cli.Exit(message)
+			}
+
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(expectedMessage)
+			Expect(err.ExitCode()).To(Equal(expectedCode))
+		},
+		Entry("string", "message", Equal("message"), 1),
+		Entry("string slice", []interface{}{"class", "ified"}, Equal("classified"), 1),
+		Entry("end with exit code", []interface{}{"b", 255}, Equal("b"), 255),
+		Entry("end with error code", []interface{}{"error", cli.UnexpectedArgument}, Equal("unexpected argument: error"), 2),
+		Entry("error and error code", []interface{}{errors.New("error"), cli.UnexpectedArgument}, Equal("unexpected argument: error"), 2),
+		Entry("already exit coder", cli.Exit("a", 255), Equal("a"), 255),
+		Entry("error", errors.New("a"), Equal("a"), 1),
+		Entry("error code", cli.UnexpectedArgument, Equal("unexpected argument"), 2),
+	)
+})
