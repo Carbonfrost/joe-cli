15ced0bc5eb41f8e2d93f343ad4b84a9f30466ba
Gary Mann <garythemann@users.noreply.github.com>
2022-07-09 00:12:03 -0700

    Fix build on Linux

--- extensions/exec/open.go
+++ extensions/exec/open.go
@@ -7,10 +7,10 @@ import (
 	"os/exec"
 )
 
-func openUsingApp(input, app string) *exec.Cmd {
+func openUsingApp(input, app string) (*exec.Cmd, error) {
 	if os.Getenv("DISPLAY") != "" {
-		return exec.Command("xdg-open", input)
+		return exec.Command("xdg-open", input), nil
 	}
 
-	return nil
+	return nil, nil
 }
