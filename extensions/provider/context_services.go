edc2fcc73a582fb26c97866a71cbb8128c8ea4e7
Gary Mann <garythemann@users.noreply.github.com>
2023-04-20 23:04:58 -0700

    Bug fix: Ensure Context is unwrapped in provider context services

--- extensions/provider/context_services.go
+++ extensions/provider/context_services.go
@@ -31,7 +31,7 @@ func WithServices(c context.Context) (context.Context, *ContextServices) {
 // Services obtains the contextual services used by the package.  If not
 // already present, it will be added to the context.
 func Services(c *cli.Context) *ContextServices {
-	newCtx, o := WithServices(c)
+	newCtx, o := WithServices(c.Context())
 	c.SetContext(newCtx)
 	return o
 }
